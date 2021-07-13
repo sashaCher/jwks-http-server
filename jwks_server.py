007fb821a672d19436f7513285d8f5cb81fc68fb
Sasha Chernomordik <sashac@cyberark.com>
2021-07-13 19:10:22 +0300

    Linting the code

--- jwks_server.py
+++ jwks_server.py
@@ -21,9 +21,9 @@ class JWKSRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(response.encode('utf-8'))
 
-    def getKey(self, keyName, alg):
-        if keyName not in keys:
-            keys[keyName] = {
+    def get_key(self, key_name, alg):
+        if key_name not in keys:
+            keys[key_name] = {
                 'RS256': jwk.JWK.generate(kty='RSA', size=1024),
                 'RS384': jwk.JWK.generate(kty='RSA', size=2048),
                 'RS512': jwk.JWK.generate(kty='RSA', size=4048),
@@ -34,27 +34,27 @@ class JWKSRequestHandler(BaseHTTPRequestHandler):
                 'HS384': jwk.JWK.generate(kty='oct', size=384),
                 'HS512': jwk.JWK.generate(kty='oct', size=512)
             }.get(alg, None)
-        return keys[keyName]
+        return keys[key_name]
 
-    def exportKey(self, key):
+    def export_key(self, key):
         try:
-            keyJson = key.export(private_key=False)
+            key_json = key.export(private_key=False)
         except jwk.InvalidJWKType:
-            keyJson = key.export()
-        keyDict = json.loads(keyJson, object_pairs_hook=OrderedDict)
-        keyDict['kid'] = key.thumbprint()
-        return keyDict
+            key_json = key.export()
+        key_dict = json.loads(key_json, object_pairs_hook=OrderedDict)
+        key_dict['kid'] = key.thumbprint()
+        return key_dict
 
-    def base64Padding(self, value):
+    def base64_padding(self, value):
         return value + "=" * (-len(value) % 4)
 
-    def decodeToken(self, token):
-        headB64, paylB64, sig = token.split(".", 3)
-        head = base64.urlsafe_b64decode(self.base64Padding(headB64))
-        payl = base64.urlsafe_b64decode(self.base64Padding(paylB64))
-        headDict = json.loads(head, object_pairs_hook=OrderedDict)
-        paylDict = json.loads(payl, object_pairs_hook=OrderedDict)
-        return headDict, paylDict
+    def decode_token(self, token):
+        head_b64, payl_b64 = token.split(".", 3)
+        head = base64.urlsafe_b64decode(self.base64_padding(head_b64))
+        payl = base64.urlsafe_b64decode(self.base64_padding(payl_b64))
+        head_dict = json.loads(head, object_pairs_hook=OrderedDict)
+        payl_dict = json.loads(payl, object_pairs_hook=OrderedDict)
+        return head_dict, payl_dict
 
     def do_GET(self):
         logging.info("GET: %s", str(self.path))
@@ -62,18 +62,18 @@ class JWKSRequestHandler(BaseHTTPRequestHandler):
 
         if len(parts) == 2:
             key_name, alg = parts[0], parts[1]
-            key = self.getKey(key_name, alg)
+            key = self.get_key(key_name, alg)
             resp = json.dumps({
                 'keys': [
-                    self.exportKey(key)
+                    self.export_key(key)
                 ]
             })
         else:
-            keyList = []
+            key_list = []
             for key in keys.values():
-                keyList.append(self.exportKey(key))
+                key_list.append(self.export_key(key))
             resp = json.dumps({
-                'keys': keyList
+                'keys': key_list
             })
 
         self.reply(resp)
@@ -86,9 +86,9 @@ class JWKSRequestHandler(BaseHTTPRequestHandler):
         logging.info("BODY %s", post_data.decode('utf-8'))
 
         key_name, alg = self.path.strip('/').split('/')
-        key = self.getKey(key_name, alg)
+        key = self.get_key(key_name, alg)
 
-        head, payl = self.decodeToken(post_data.decode('utf-8'))
+        head, payl = self.decode_token(post_data.decode('utf-8'))
         head['alg'] = alg
         head['kid'] = key.thumbprint()
 
@@ -100,8 +100,7 @@ class JWKSRequestHandler(BaseHTTPRequestHandler):
         logging.info("DELETE %s", str(self.path))
         parts = self.path.strip('/').split('/')
         if len(parts) == 2:
-            key_name, alg = parts[0], parts[1]
-            del keys[key_name]
+            del keys[parts[0]]
         else:
             keys.clear()
         self.reply("DELETED")
@@ -121,6 +120,11 @@ def run(port, server_class=HTTPServer, handler_class=JWKSRequestHandler):
 
 logging.basicConfig(level=logging.INFO)
 parser = argparse.ArgumentParser(description='Lightweight JWKS server')
-parser.add_argument('-p', '--port', type=int, help="defines http server port (default: {})".format(DEFAULT_PORT), default=DEFAULT_PORT)
+parser.add_argument(
+    '-p',
+    '--port',
+    type=int,
+    default=DEFAULT_PORT,
+    help="defines http server port (default: {})".format(DEFAULT_PORT))
 args = parser.parse_args()
 run(args.port)
